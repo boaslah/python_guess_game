c28d1e469ad62721160460f080178edc05a97388
Blessing Boaslah <blessing@kwagei.com>
2023-03-31 16:42:24 +0000

    updated guess game

--- guess_game.py
+++ guess_game.py
@@ -1,11 +1,18 @@
 import random
 
+counter = 5
 
-print("Hello, Welcome to bboaslah's Guess Game.\nGuess a random number between 0 - 100\nand I'll let you know if your guess was wrong or correct");
-random = random.randint(0, 100);
-user_input = int(input("Please enter your guess: "));
 
-if(user_input == random):
-    print("Congratulations! Your guess was right!");
-else:
-    print("Your guess was wrong.", random, "is the random is the random number." );
+print("Hello, Welcome to bboaslah's Guess Game.\nGuess a random number between 0 - 100\nand I'll let you know if your guess was wrong or correct")
+random = random.randint(0, 100)
+
+while(counter > 0):
+    user_input = int(input("Please enter your guess: "))
+    if(user_input == random):
+        print("Congratulations! Your guess was right!")
+        break
+    elif(user_input > random):
+        print("Your guess is too high")
+    elif(user_input < random):
+        print("Your guess is too low", random);
+    counter = counter -1
