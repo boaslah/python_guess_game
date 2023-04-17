cc2a0e8b5faca612495023cebc8db084f78c7080
boaslah <blessing@kwagei.com>
2023-04-17 09:15:21 +0000

    updated the code

--- guess_game.py
+++ guess_game.py
@@ -3,10 +3,10 @@ import random
 counter = 5
 
 
-print("Hello, Welcome to bboaslah's Guess Game.\nGuess a random number between 0 - 100\nand I'll let you know if your guess was wrong or correct")
+print("Hello, Welcome to the ODC Guess Game.\nYou have 5 chances to guess a random number between 0 - 100")
 random = random.randint(0, 100)
 
-while(counter > 0):
+while(counter):
     user_input = int(input("Please enter your guess: "))
     if(user_input == random):
         print("Congratulations! Your guess was right!")
@@ -14,5 +14,7 @@ while(counter > 0):
     elif(user_input > random):
         print("Your guess is too high")
     elif(user_input < random):
-        print("Your guess is too low", random);
+        print("Your guess is too low")
     counter = counter -1
+        
+print("You have reached the end of your chances. The right guess is", random)
